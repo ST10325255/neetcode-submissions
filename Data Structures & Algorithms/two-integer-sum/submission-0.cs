93707cc1531583801a4fa8b7d737dd94f1714960
Zeke <126753620+ST10325255@users.noreply.github.com>
2026-08-05 13:58:19 +0200

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var seen = new Dictionary<int, int>(); // value -> index
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            
+            if (seen.TryGetValue(complement, out int j))
+            {
+                return new int[] { j, i };
+            }
+            
+            seen[nums[i]] = i;
+        }
+        
+        throw new ArgumentException("No valid pair found.");
+    }
+}
